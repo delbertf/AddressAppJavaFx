ac6a8e284ec35c3a8c4f3a3bacfc6b156399bf3f
delbertf <friesen.delbert@gmail.com>
2016-11-02 12:46:33 +0100

    Building sceen for PersonOverview and RootLayout

---
+++ src/ch/dfr/address/MainApp.java
@@ -0,0 +1,79 @@
+package ch.dfr.address;
+/**
+ * Created by dfr on 02.11.2016.
+ * This is the main app class
+ */
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+
+import java.io.IOException;
+
+public class MainApp extends Application {
+
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("AddressApp");
+
+        initRootLayout();
+
+        showPersonOverview();
+    }
+
+    /**
+     * Initializes the root layout.
+     */
+    private void initRootLayout() {
+        try {
+            // load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+            this.rootLayout = loader.load();
+
+            // show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Shows the person overview inside the root layout.
+     */
+    private void showPersonOverview() {
+        try {
+            // load person overview
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
+            AnchorPane personOverview = loader.load();
+
+            // set person overview into center of the root layout.
+            rootLayout.setCenter(personOverview);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Returns the main stage.
+     * @return Stage
+     */
+    public Stage getPrimaryStage() {
+        return this.primaryStage;
+    }
+}
