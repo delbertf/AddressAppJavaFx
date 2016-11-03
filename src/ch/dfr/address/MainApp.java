232fe729fe510ba05114e7cd7437ce978f5e31fc
delbertf <friesen.delbert@gmail.com>
2016-11-03 17:04:28 +0100

    Add CSS style

--- src/ch/dfr/address/MainApp.java
+++ src/ch/dfr/address/MainApp.java
@@ -12,6 +12,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
@@ -55,6 +56,9 @@ public class MainApp extends Application {
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("AddressApp");
+
+        // set the application icon
+        this.primaryStage.getIcons().add(new Image("file:resources/images/Address_Book_32.png"));
 
         initRootLayout();
 
